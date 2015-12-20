d5fceb646f2ba663ed45a82ba37e9fe94450d2fe
Benjamin Coe <ben@npmjs.com>
2015-12-19 21:31:38 -0800

    make setters work too

--- test.js
+++ test.js
@@ -43,4 +43,26 @@ describe('any-test', function () {
     o['README.md'].should.equal(99)
     return done()
   })
+
+  it('updates all permutations when a simple value is changed', function (done) {
+    var o = anyPath({
+      '.\\foo\\bar\\README.md': 99
+    })
+    o['./foo/bar/README.md'] = 202
+
+    o['./foo\/bar/README.md'].should.equal(202)
+    o['.\\foo\\bar\\README.md'].should.equal(202)
+    return done()
+  })
+
+  it("updates all permutations when an inner object's value is changed", function (done) {
+    var o = anyPath({
+      '.\\foo\\bar\\README.md': {name: 'README.md'}
+    })
+    o['./foo/bar/README.md'].name = 'package.json'
+
+    o['./foo\/bar/README.md'].name.should.equal('package.json')
+    o['.\\foo\\bar\\README.md'].name.should.equal('package.json')
+    return done()
+  })
 })
